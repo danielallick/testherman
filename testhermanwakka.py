96804fba3422806fc13d73e63ac773f639ce7320
Daniel Allick <daniel.allick@hotmail.com>
2021-01-07 15:29:43 +0100

    no message

---
+++ testhermanwakka.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 14:35:25 2021
+
+@author: danielallick
+"""
+#estherman wakka wakka ey ey
+
+
+#check dit is mijn update
+
+extra comment 
