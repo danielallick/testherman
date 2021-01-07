12abb7152078b1212f48209a48afc5316638647e
Herman Martens <herman.martens@hotmail.com>
2021-01-07 15:26:41 +0100

    Merge branch 'main' of https://github.com/danielallick/testherman into main
    # Conflicts:
    #	testhermanwakka.py

--- testhermanwakka.py
+++ testhermanwakka.py
@@ -5,9 +5,15 @@ Created on Wed Jan  6 14:35:25 2021
 
 @author: danielallick
 """
+<<<<<<< HEAD
+testherman wakka wakka ey ey
+
+tjak
+=======
 #estherman wakka wakka ey ey
 
 
 #check dit is mijn update
 
 extra comment 
+>>>>>>> 792b99ae01bb5a4f7e53390b4ffafd83bd55b5df
